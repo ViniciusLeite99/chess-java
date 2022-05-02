043b0c3796a08d4ba2235951a75d3a1be492a34c
Vinicius Leite <v.s.leite@hotmail.com>
2022-05-02 15:49:35 -0300

    Fist class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,27 +2,17 @@ package application;
 
 import java.util.Scanner;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
+		Position pos = new Position(3,5);
+		System.out.println(pos);
 		
-		System.out.println("Olá, o que vc quer?");
-		String pessoa  = scan.next();
 		
-		if(pessoa  == "Grana"){
-			System.out.println("Vai trabalhar, sem vergonha");
-		}
-		else if (pessoa == "Diversão") {
-			System.out.println("Então o que vc está fazendo aqui?, vai viajar, curtira vida...sei lá");
-		}
-		else if(pessoa == "Estudar") {
-			System.out.println("Um exemplo de pessoa, um futuro sábio em nosso meio");
-		}
-		else {
-			System.out.println("Não quero saber");
-		}
 		
 	}
 
